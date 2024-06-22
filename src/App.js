c7edb707f3444a90d9b02fb166b4a34857a1bee1
rekamakadi <r.makadi@gmail.com>
2024-06-22 17:05:21 +0200

    [feat] create Contact section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Skills } from './components/Skills';
 import { Projects } from './components/Projects';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { Contact } from './components/Contact';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Banner />
       <Skills />
       <Projects />
+      <Contact />
     </div>
   );
 }

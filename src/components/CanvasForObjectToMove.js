5b98b529b79d543a9b750f1e385af7586bb48d45
rekamakadi <r.makadi@gmail.com>
2024-07-07 15:21:35 +0200

    [change] optimize movement's settings

--- src/components/CanvasForObjectToMove.js
+++ src/components/CanvasForObjectToMove.js
@@ -1,33 +1,16 @@
-import React, { useRef, useEffect } from "react";
+import React from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import { ObjectToMove } from "./ObjectToMove";
 
 export const CanvasForObjectToMove = ({ objectName }) => {
-  const mousePosition = useRef({
-    x: window.innerWidth / 2,
-    y: window.innerHeight / 2,
-  });
-
-  useEffect(() => {
-    const handleMouseMove = (event) => {
-      mousePosition.current.x = event.clientX;
-      mousePosition.current.y = event.clientY;
-    };
-
-    window.addEventListener("mousemove", handleMouseMove);
-
-    return () => {
-      window.removeEventListener("mousemove", handleMouseMove);
-    };
-  }, []);
 
   return (
     <div id="container3D" style={{ width: "100%", height: "100vh" }}>
       <Canvas style={{ width: "100%", height: "100%", cursor: "pointer" }}>
         <ambientLight intensity={1} />
         <directionalLight position={[5, 5, 5]} />
-        <ObjectToMove objectName={objectName} mousePosition={mousePosition} />
+        <ObjectToMove objectName={objectName} />
         <OrbitControls enableZoom={true} />
       </Canvas>
     </div>
--- src/components/ObjectToMove.js
+++ src/components/ObjectToMove.js
@@ -2,7 +2,7 @@ import React, { useRef } from "react";
 import { useFrame } from "@react-three/fiber";
 import { useGLTF } from "@react-three/drei";
 
-export const ObjectToMove = ({ objectName, mousePosition }) => {
+export const ObjectToMove = ({ objectName }) => {
   const { scene } = useGLTF(
     `${process.env.PUBLIC_URL}/models/${objectName}/scene.gltf`
   );
@@ -10,14 +10,8 @@ export const ObjectToMove = ({ objectName, mousePosition }) => {
 
   useFrame(() => {
     if (objectRef.current) {
-      objectRef.current.rotation.y += 0.001; // Automatic rotation
-      objectRef.current.rotation.x += 0.001; // Automatic rotation
-
-      // Mouse movement adjustment
-      objectRef.current.rotation.y +=
-        (mousePosition.current.x / window.innerWidth) * 0.03;
-      objectRef.current.rotation.x +=
-        (mousePosition.current.y / window.innerHeight) * 0.03;
+      objectRef.current.rotation.y += 0.005; // Automatic rotation
+      objectRef.current.rotation.x += 0.005; // Automatic rotation
     }
   });
 

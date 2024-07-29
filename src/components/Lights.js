320e746802263d50c23592c6f4fbedc72f3e0a51
rekamakadi <r.makadi@gmail.com>
2024-07-29 14:13:41 +0200

    [change] cube model

--- src/components/Lights.js
+++ src/components/Lights.js
@@ -1,19 +1,34 @@
 // src/components/Lights.js
 
 import React from 'react';
-import { DirectionalLight, AmbientLight } from 'three';
 
 function Lights() {
   return (
+    
     <>
-      <ambientLight intensity={1} />
-      <directionalLight position={[10, 10, 5]} intensity={1} />
 
+  {/* directions x/y/z */}
 
+      <ambientLight intensity={1} /> {/* Reduced intensity to balance with other lights */}
+       <hemisphereLight skyColor={0xc1ff2d} groundColor={0x9393ff} intensity={1} /> {/* Hemisphere light for a more natural look
+    {/* <hemisphereLight skyColor={0xffffbb} groundColor={0x080820} intensity={1.6} />  Hemisphere light for a more natural look  */}
+     
+     
+     
+     <directionalLight position={[10, 10, 10]} intensity={0.5} />   {/* right */}
+     <directionalLight position={[-10, 10, 10]} intensity={0.5} />  {/* left  */}
+     <directionalLight position={[0, 0, 10]} intensity={0.5} />     {/* Front */}
+     <directionalLight position={[0, 0, -10]} intensity={0.5} />    {/* back  */}
 
+     <directionalLight position={[-5, -20, 0]} intensity={1.5} />    {/* under */}
+     <directionalLight position={[5, 20, 0]} intensity={1.0} />     {/* above */}
 
+      <pointLight position={[0, -10, 0]} intensity={0.5} />  {/*Additional point light */}
+       {/*<spotLight position={[0, 5, 5]} intensity={2} angle={0.3} penumbra={0.1} />  Additional spot light */}
+      
     </>
   );
 }
 
 export default Lights;
+
--- src/modelConfig.js
+++ src/modelConfig.js
@@ -1,14 +1,15 @@
 const models = [
-  {
-    path: `${process.env.PUBLIC_URL}/models/cube/cube.gltf`,
-    //rotationSpeed: { x: 0.050, y: 0.050 },
-    //cameraSettings: { position: [0, 0, 5], fov: 20 } // Cube settings
-  },
-  {
-    path: `${process.env.PUBLIC_URL}/models/can/can.gltf`,
-    //rotationSpeed: { x: 0.050, y: 0.050 },
-    //cameraSettings: { position: [0, 0, 5], fov: 50 } // Can settings
-  },
-];
-
-export default models;
+    { 
+      path: `${process.env.PUBLIC_URL}/models/cube/cube.gltf`,
+      //rotationSpeed: { x: 0.0020, y: -0.002 },
+      //cameraSettings: { position: [0, 0, 5], fov: 20 } // Cube settings
+    },
+    { 
+      path: `${process.env.PUBLIC_URL}/models/can/can.gltf`,
+      //rotationSpeed: { x: 0.00, y: 0.00 },
+      cameraSettings: { position: [0, 0, 5], fov: 45 } // Can settings
+    }
+  ];
+  
+  export default models;
+  

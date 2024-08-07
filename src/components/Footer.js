e8c2ab5dafb1c56dc4a54a21d31af0046636f855
rekamakadi <r.makadi@gmail.com>
2024-08-07 11:22:16 +0200

    [change] edit footer css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,4 @@
 import { Container, Row, Col } from "react-bootstrap";
-// import logo from "../assets/img/logo.svg";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
@@ -10,7 +9,12 @@ export const Footer = () => {
       <Container>
         <Row className="align-items-center">
           <Col size={12} sm={6}>
-            <img src={""} alt="Logo" />
+            <a href="/">
+              <img
+                src={`${process.env.PUBLIC_URL}/sdv-LOGO-03.svg`}
+                alt="Logo"
+              />
+            </a>
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
@@ -19,7 +23,7 @@ export const Footer = () => {
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <img src={navIcon1} alt="linkedin icon" />
+                <img src={navIcon1} alt="linkedin icon"/>
               </a>
               <a
                 href="https://www.facebook.com/zsoltalfred.molnarbaka"
